4d61a9c83eb531fc7ff0236e859692575412cd24
Deivintir <d4vidyeva@gmail.com>
2024-01-27 08:07:26 +0100

    Funciones async en JavaScript

---
+++ js/main.js
@@ -0,0 +1,50 @@
+//IMPLEMENTACIÓN DEL PATRÓN "ASYNC-WAIT".
+/*En esta unidad vamos a avanzar en el conocimiento de la asincronía gracias al aprendizaje del patrón "async-wait,
+disponible desde ECMAScript2017 y que ha tenido un gran éxito en su implementación en todo tipo de soluciones.
+Para concluir, aprendremos cómo podemos manejar los errores generados por las promesas utilizadas en este patrón
+con la implementación de bloques "try-catch".*/
+
+/*Patrón "async-wait:
+Es una característica sintáctica de muchos lenguajes para la ejecución de instrucciones no bloqueantes del programa
+que permiten implementar bloques long-running(tardan mucho en ejecutrase); el programa, mientras, continúa la ejecución
+del resto de instrucciones.
+El objetivo fundamental de este patrón es que los desarrolladores puedan escribir código asíncrono como si fuera síncrono, 
+simplificando el uso tanto de funciones callback como de promesas, y es una alternaiva a estas últimas con un código más 
+legible y de fácil mantenimiento.*/
+    function getUser(){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve({name: 'Sara', language: 'es'}), 2000)
+        })
+    }
+//Declaramos un proceso asíncrono que tardará dos segundos en ejecutarse.
+/*Se implementa el patrón "async-await" declarando una función precedida de la palabra reservada "async",
+que le especificará al intérprete JavaScript que dentro del cuerpo de esa función se ejecutarán procesos asíncronos.
+La sintaxis se complementa con el uso de la palabra reservada "await" en las instruciones que ejecuten un proceso asíncrono.*/
+async function setMessage() {
+    let user = await getUser();
+    console.log(user.name);
+}
+setMessage();
+/*Además,podemos combinar denro de la función asíncrona "setMessage" el uso de promesas con instrucciones asíncronas.
+Ejemplo: */
+function getPilot(){
+    return new Promise ((resolve, reject)=>{
+        setTimeout(()=>resolve({name:'Max', language:'en'}),2000)
+    })
+}
+
+function getMessage(language){
+    if(language === 'es'){
+        return 'Hola;'
+    }
+    if(language === 'en'){
+        return 'Hello';
+    }
+}
+
+async function messageSet(){
+    let pilot=await getPilot();
+    let message = getMessage(pilot.language);
+    console.log(message, pilot.name);
+}   
+messageSet();
